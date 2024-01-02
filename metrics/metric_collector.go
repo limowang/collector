8e5a37d83c49e384fbba11cbd35bd94ad0aa2fdf
wangguangshuo <1924240046@qq.com>
2024-01-02 03:49:21 +0000

    In the version developed on 12-29, the label settings need to be changed a lot later to facilitate rollback.

--- metrics/metric_collector.go
+++ metrics/metric_collector.go
@@ -44,13 +44,14 @@ type Metric struct {
 	value float64
 	// For metric type of percentile.
 	values []float64
+	
 	mtype  string
 }
 
 type Metrics []Metric
 
 //针对p50,p90等级别定义层级
-//var TaskLevel = [5]string{"p50","p90","p95","p99","p999"}
+var TaskLevel = [5]string{"p50","p90","p95","p99","p999"}
 var GaugeMetricsMap map[string]prometheus.GaugeVec
 var CounterMetricsMap map[string]prometheus.CounterVec
 var SummaryMetricsMap map[string]prometheus.Summary
@@ -397,6 +398,7 @@ func processAllServerMetrics(dsource int) {
 	metricsByTableID := make(map[string]Metrics, 128)
 	metricsByServerTableID := make(map[string]Metrics, 128)
 	metricsByTaskName := make(map[string]Metrics, 128)  	//针对profiler类型进行处理
+	metricsOfReplicaByTableID := make(map[string]Metrics,128) 	//针对replica类型进行处理
 	var metricsOfCluster []Metric
 	metricsByAddr := make(map[string]Metrics, 128)
 	for _, addr := range addrs {
@@ -410,15 +412,20 @@ func processAllServerMetrics(dsource int) {
 			etype := entity.Get("type").String()
 			switch etype {
 			case "replica":
+				//工作待完成，这个大类里面有Counter,Gauge,Percentile类型指标  12-27
+				tableID := entity.Get("attributes").Get("table_id").String()
+				collectReplicaSeverLevelTableMetric(entity.Get("metrics").Array(),tableID,
+					&metricsOfReplicaByTableID)
 			case "profiler":
 				taskName := entity.Get("attributes").Get("task_name").String()
 				mergeIntoServerLevelTaskMetrics(entity.Get("metrics").Array(),
 					taskName,&metricsByTaskName)
+				updateServerLevelTaskMetrics(addr,metricsByTaskName,dsource)
 			case "partition":
 				tableID := entity.Get("attributes").Get("table_id").String()
 				mergeIntoClusterLevelTableMetric(entity.Get("metrics").Array(),
 					tableID, &metricsByTableID)
-				updateServerLevelTaskMetrics(addr,metricsByTaskName,dsource)
+				//updateServerLevelTaskMetrics(addr,metricsByTaskName,dsource)
 			case "table":
 				tableID := entity.Get("attributes").Get("table_id").String()
 				mergeIntoClusterLevelTableMetric(entity.Get("metrics").Array(),
@@ -435,12 +442,41 @@ func processAllServerMetrics(dsource int) {
 				log.Errorf("Unsupport entity type %s", etype)
 			}
 		}
+
+		//replica分片是replica-server才具有的，所有指标汇总完成之后进行prometheus数据更新，这个位置放置是否合理后面可能要进行确认修改
+		updateReplicaServerLevelTableMetric(addr,metricsOfReplicaByTableID,dsource)
 	}
 
-	//updateServerLevelTaskMetrics(metricsByTaskName,dsource)
+	//updateServerLevelTaskMetrics(addr,metricsByTaskName,dsource)
 	updateClusterLevelTableMetrics(metricsByTableID,dsource)
 	updateServerLevelServerMetrics(metricsByAddr,dsource)
 	updateClusterLevelMetrics(metricsOfCluster,dsource)
+}
+
+//将replica数据更新到prometheus集合中，方便后面采集
+func updateReplicaServerLevelTableMetric(addr string,metricsOfReplicaByTableID map[string]Metrics,dsource int) {
+	for tableID, metrics := range metricsOfReplicaByTableID {
+		var tableName string
+		if name, ok := TableNameByID[tableID]; !ok {
+			tableName = tableID
+		}else {
+			tableName = name
+		}
+
+		for _, metric := range metrics {
+			mtype := metric.mtype
+			switch mtype {
+			case "Counter":
+				updateMetric(metric, addr, "table", tableName,dsource)
+			case "Gauge":
+				updateMetric(metric, addr, "table", tableName,dsource)
+			case "Percentile":
+				updateTaskMetric(metric,addr,tableName,dsource)
+			default:
+				log.Warnf("Unsupport metric type %s", metric.mtype)
+			}
+		}
+	}
 }
 
 //将profiler数据更新到prometheus集合中，方便后面采集
@@ -477,39 +513,43 @@ func updateTaskMetric(metric Metric,endpoint string,title string,dsource int) {
 		}
 	case "Percentile":
 		//log.Warnf("Todo metric type %s", metric.mtype)
-		if gauge, ok := GaugeMetricsMap[metric.name];ok {
+		if gauge, ok := GaugeMetricsMap[metric.name]; ok {
 			//各个级别的数据依次更新
-			// for i := 0; i < 5; i++ {
-			// 	gauge.With(
-			// 		prometheus.Labels{"endpoint": endpoint,
-			// 			"role": role, "level": TaskLevel[i],
-			// 			"title": title}).Set(float64(metric.values[i]))
+			for i := 0; i < 5; i++ {
+				gauge.With(
+					prometheus.Labels{"endpoint": endpoint,
+						"role": role, "level": TaskLevel[i],
+						"title": title}).Set(float64(metric.values[i]))
+			}
+			//fmt.Printf("values[0]:%f \n",metric.values[0])
+			// if(len(metric.values) == 0) {
+			// 	break
 			// }
-			p := float64(metric.values[0])
-			gauge.With(
-				prometheus.Labels{"endpoint": endpoint,
-					"role": role, "level": "p50",
-					"title": title}).Set(p)
-			p = float64(metric.values[1])
-			gauge.With(
-				prometheus.Labels{"endpoint": endpoint,
-					"role": role, "level": "p90",
-					"title": title}).Set(p)
-			p = float64(metric.values[2])
-			gauge.With(
-				prometheus.Labels{"endpoint": endpoint, 
-					"role": role, "level": "p95",
-					"title": title}).Set(p)
-			p = float64(metric.values[3])
-			gauge.With(
-				prometheus.Labels{"endpoint": endpoint,
-					"role": role, "level": "p99",
-					"title": title}).Set(p)
-			p = float64(metric.values[4])
-			gauge.With(
-				prometheus.Labels{"endpoint": endpoint,
-					"role": role, "level": "p999",
-					"title": title}).Set(p)
+			// p := float64(metric.values[0])
+			// gauge.With(
+			// 	prometheus.Labels{"endpoint": endpoint,
+			// 		"role": role, "level": "p50",
+			// 		"title": title}).Set(p)
+			// p = float64(metric.values[1])
+			// gauge.With(
+			// 	prometheus.Labels{"endpoint": endpoint,
+			// 		"role": role, "level": "p90",
+			// 		"title": title}).Set(p)
+			// p = float64(metric.values[2])
+			// gauge.With(
+			// 	prometheus.Labels{"endpoint": endpoint, 
+			// 		"role": role, "level": "p95",
+			// 		"title": title}).Set(p)
+			// p = float64(metric.values[3])
+			// gauge.With(
+			// 	prometheus.Labels{"endpoint": endpoint,
+			// 		"role": role, "level": "p99",
+			// 		"title": title}).Set(p)
+			// p = float64(metric.values[4])
+			// gauge.With(
+			// 	prometheus.Labels{"endpoint": endpoint,
+			// 		"role": role, "level": "p999",
+			// 		"title": title}).Set(p)
 		}else {
 			log.Warnf("Unknown metric name %s", metric.name)
 		}
@@ -603,7 +643,7 @@ func collectServerLevelTableMetric(metrics []gjson.Result, tableID string,
 		mtype := metric.Get("type").String()
 		value := metric.Get("value").Float()
 		var values []float64
-		if mtype == "percentile" {
+		if mtype == "Percentile" {
 			values = append(values, metric.Get("p50").Float())
 			values = append(values, metric.Get("p90").Float())
 			values = append(values, metric.Get("p95").Float())
@@ -624,7 +664,8 @@ func collectServerLevelServerMetrics(metrics []gjson.Result, addr string,
 		mtype := metric.Get("type").String()
 		value := metric.Get("value").Float()
 		var values []float64
-		if mtype == "percentile" {
+		if mtype == "Percentile" {
+
 			values = append(values, metric.Get("p50").Float())
 			values = append(values, metric.Get("p90").Float())
 			values = append(values, metric.Get("p95").Float())
@@ -635,6 +676,62 @@ func collectServerLevelServerMetrics(metrics []gjson.Result, addr string,
 		mts = append(mts, m)
 	}
 	(*metricsByAddr)[addr] = mts
+}
+
+//周期性地将获取的指标存储并更新到prometheus接口数据
+func collectReplicaSeverLevelTableMetric(metrics []gjson.Result,tableID string,
+	metricsOfReplicaByTableID *map[string]Metrics) {
+		if _, ok := (*metricsOfReplicaByTableID)[tableID]; ok {
+			//已经存在进行更新
+			mts := (*metricsOfReplicaByTableID)[tableID]
+			for _, metric := range metrics {
+				name := metric.Get("name").String()
+				mtype := metric.Get("type").String()
+				value := metric.Get("value").Float()
+				for _, m := range mts {
+					if name == m.name {
+						switch mtype {
+						case "Counter":
+							m.value += value			//同一个分片的不同replica如何进行指标的汇聚操作？？？？？？？
+						case "Gauge":
+							m.value += value
+						case "Percentile":
+							p50 := metric.Get("p50").Float()
+							m.values[0] = math.Max(m.values[0], p50)		//这个地方的数据更新好像也有问题？？？？ -------> 后面可能要修改
+							p90 := metric.Get("p90").Float()
+							m.values[1] = math.Max(m.values[0], p90)
+							p95 := metric.Get("p95").Float()
+							m.values[2] = math.Max(m.values[0], p95)
+							p99 := metric.Get("p99").Float()
+							m.values[3] = math.Max(m.values[0], p99)
+							p999 := metric.Get("p999").Float()
+							m.values[4] = math.Max(m.values[0], p999)
+						case "Histogram":
+						default:
+							log.Errorf("Unsupport metric type %s", mtype)
+						}
+					}
+				}
+			}
+		} else {
+			var mts Metrics
+			for _, metric := range metrics {
+				name := metric.Get("name").String()
+				mtype := metric.Get("type").String()
+				value := metric.Get("value").Float()
+				var values []float64
+				if mtype == "Percentile" {
+					values = append(values, metric.Get("p50").Float())
+					values = append(values, metric.Get("p90").Float())
+					values = append(values, metric.Get("p95").Float())
+					values = append(values, metric.Get("p99").Float())
+					values = append(values, metric.Get("p999").Float())
+				}
+				m := Metric{name: name, mtype: mtype, value: value, values: values}
+				mts = append(mts, m)
+			}
+			(*metricsOfReplicaByTableID)[tableID] = mts
+		}
 }
 
 //profiler的percentile类型数据合并收集并存
@@ -654,6 +751,9 @@ func mergeIntoServerLevelTaskMetrics(metrics []gjson.Result, taskName string,
 						case "Gauge":
 							m.value += value
 						case "Percentile":				//percentile类型的更新是否正确？
+							// if(len(m.values) == 0) {
+							// 	break
+							// }
 							p50 := metric.Get("p50").Float()
 							m.values[0] = math.Max(m.values[0], p50)
 							p90 := metric.Get("p90").Float()
@@ -677,14 +777,17 @@ func mergeIntoServerLevelTaskMetrics(metrics []gjson.Result, taskName string,
 				name := metric.Get("name").String()
 				mtype := metric.Get("type").String()
 				value := metric.Get("value").Float()
+
+				//m := Metric{name: name, mtype: mtype, value: value}
 				var values []float64
-				if mtype == "percentile" {
+				if mtype == "Percentile" {
 					values = append(values, metric.Get("p50").Float())
 					values = append(values, metric.Get("p90").Float())
 					values = append(values, metric.Get("p95").Float())
 					values = append(values, metric.Get("p99").Float())
 					values = append(values, metric.Get("p999").Float())
 				}
+				//copy(m.values,values)
 				m := Metric{name: name, mtype: mtype, value: value, values: values}
 				mts = append(mts, m)
 			}
@@ -725,7 +828,7 @@ func mergeIntoClusterLevelServerMetric(metrics []gjson.Result, metricsOfCluster 
 		if !isExisted {
 			value := metric.Get("value").Float()
 			var values []float64
-			if mtype == "percentile" {
+			if mtype == "Percentile" {
 				values = append(values, metric.Get("p50").Float())
 				values = append(values, metric.Get("p90").Float())
 				values = append(values, metric.Get("p95").Float())
@@ -779,7 +882,7 @@ func mergeIntoClusterLevelTableMetric(metrics []gjson.Result, tableID string,
 			mtype := metric.Get("type").String()
 			value := metric.Get("value").Float()
 			var values []float64
-			if mtype == "percentile" {
+			if mtype == "Percentile" {
 				values = append(values, metric.Get("p50").Float())
 				values = append(values, metric.Get("p90").Float())
 				values = append(values, metric.Get("p95").Float())
